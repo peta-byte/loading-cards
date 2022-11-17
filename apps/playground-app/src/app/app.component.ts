b322d9713ce9b4d255401ba1ac154a91f13b9780
Chepkorir Brenda <40006302+peta-byte@users.noreply.github.com>
2022-11-16 20:36:02 -0600

    Update readme (#2)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * update

--- apps/playground-app/src/app/app.component.ts
+++ apps/playground-app/src/app/app.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { lastValueFrom, Observable } from 'rxjs';
-import { Usr, GraphQLResponse } from '@graphql-playground/api-interfaces';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'graphql-playground-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  users: Usr[] = [];
-
-  user$: Observable<GraphQLResponse> = this.http.get<GraphQLResponse>('/api/user')
-  users$: Observable<GraphQLResponse> = this.http.get<GraphQLResponse>('/api/users')
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    lastValueFrom(this.users$).then((res) => {
-      this.users = res.data.users ?? []
-    })
-  }
-}
+export class AppComponent {}
--- apps/playground-app/src/app/app.module.ts
+++ apps/playground-app/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GenderComponent } from './gender/gender.component';
 import { NationalitiesComponent } from './nationalities/nationalities.component';
-import { CommonModule } from '@angular/common';
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
+import { RouteReuseStrategy } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, GenderComponent, NationalitiesComponent],
-  imports: [CommonModule,BrowserModule, HttpClientModule, IonicModule.forRoot()],
-  providers: [],
+  declarations: [
+    AppComponent,
+    GenderComponent,
+    NationalitiesComponent,
+    HomeComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    IonicModule.forRoot(),
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
