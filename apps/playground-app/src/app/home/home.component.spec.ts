56bc6ef58577a53da9fcd37b10108a176544b3f1
Chepkorir Brenda <40006302+peta-byte@users.noreply.github.com>
2022-12-01 19:18:07 -0600

    unit test update (#4)

--- apps/playground-app/src/app/home/home.component.spec.ts
+++ apps/playground-app/src/app/home/home.component.spec.ts
@@ -1,19 +1,29 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { AppService } from '../app.service';
 
 import { HomeComponent } from './home.component';
+
+jest.mock('../app.service');
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let service: AppService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [HomeComponent],
+      providers: [provideMockStore(), AppService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = fixture.debugElement.injector.get(AppService);
   });
 
   it('should create', () => {
--- apps/playground-app/src/app/nationalities/nationalities.component.spec.ts
+++ apps/playground-app/src/app/nationalities/nationalities.component.spec.ts
@@ -1,19 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { NationalitiesComponent } from './nationalities.component';
+import { AppService } from '../app.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+jest.mock('../app.service');
 
 describe('NationalitiesComponent', () => {
   let component: NationalitiesComponent;
   let fixture: ComponentFixture<NationalitiesComponent>;
+  let service: AppService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [NationalitiesComponent],
+      providers: [provideMockStore(), AppService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NationalitiesComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = fixture.debugElement.injector.get(AppService)
   });
 
   it('should create', () => {
